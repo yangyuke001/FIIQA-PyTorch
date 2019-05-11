9fc7933908b2e03875f498374d751edb11cd43ae
杨宇克 <yangyuke001@126.com>
2019-05-11 15:20:36 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -11,19 +11,19 @@ class AGLoss(nn.Module):
     def __init__(self):
         super(AGLoss, self).__init__()
 
-    def forward(self, age_preds, age_targets):
-        '''Compute loss (age_preds, age_targets) .
+    def forward(self, fiiqa_preds, fiiqa_targets):
+        '''Compute loss (fiiqa_preds, fiiqa_targets) .
 
         Args:
-          age_preds: (tensor) predicted ages, sized [batch_size,100].
-          age_targets: (tensor) target ages, sized [batch_size,].
+          fiiqa_preds: (tensor) predicted fiiqa, sized [batch_size,100].
+          fiiqa_targets: (tensor) target fiiqa, sized [batch_size,].
 
         loss:
-          (tensor) loss = SmoothL1Loss(age_preds, age_targets)
+          (tensor) loss = SmoothL1Loss(fiiqa_preds, fiiqa_targets)
         '''
-        age_prob = F.softmax(age_preds)
-        #print('age_prob: %.3f ' % (age_prob))
-        age_expect = torch.sum(Variable(torch.range(0,199)).cuda().float()*age_prob, 1)
-        age_loss = F.smooth_l1_loss(age_expect, age_targets.float())
-        #print('age_loss: %.3f ' % age_loss.data[0])
-        return age_loss 
+        fiiqa_prob = F.softmax(fiiqa_preds,dim=1)
+        #print('fiiqa_prob: %.3f ' % (fiiqa_prob))
+        fiiqa_expect = torch.sum(Variable(torch.arange(0,200)).cuda().float()*fiiqa_prob, 1) #.float()
+        fiiqa_loss = F.smooth_l1_loss(fiiqa_expect, fiiqa_targets.float())#.float()
+        #print('fiiqa_loss: %.3f ' % fiiqa_loss.data[0])
+        return fiiqa_loss 
